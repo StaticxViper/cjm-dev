e6f7fe1cbc7b0a248aa7e25d20f2c986a16c66bc
CJ Moulton <58715830+StaticxViper@users.noreply.github.com>
2026-03-04 04:48:34 +0000

    Added logging to api manager

--- shared/api_manager.py
+++ shared/api_manager.py
@@ -1,17 +1,21 @@
 import httpx
+from logger import setup_logger
+
+logger = setup_logger(
+    name="api-manager",
+    console_levels=["INFO", "ERROR", "CRITICAL"]  # Only these show in console, any of them can be removed.
+)
 
 class APIManager:
 
     def __init__(self, url=None):
+        self.log = logger
+
         self.default_url = 'https://httpbin.org'
         if url == None:
             url = self.default_url
-        
-        r = httpx.get(url)
-        print(r)
-
-
-
+            r = httpx.get(url)
+            self.log.info(r.text)
 
 
 
