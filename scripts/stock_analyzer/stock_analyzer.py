dedd9581921e9044cf688e91406acba142c22f91
CJ Moulton <58715830+StaticxViper@users.noreply.github.com>
2026-03-19 11:24:08 +0000

    Added date param for stock api call

--- scripts/stock_analyzer/stock_analyzer.py
+++ scripts/stock_analyzer/stock_analyzer.py
@@ -5,8 +5,8 @@ from pathlib import Path
 repo_root = Path(__file__).resolve().parents[2]  # stock_analyzer.py -> stock_analyzer/ -> scripts/ -> repo_root
 sys.path.insert(0, str(repo_root))
 
+from datetime import date
 from shared.api_manager import APIManager as api
-import json
 from shared.logger import setup_logger
 
 logger = setup_logger(
@@ -27,7 +27,16 @@ def main():
     logger.critical(f"Current Stock Watchlist extracted: {result['tickers']}")
 
     # Call API Manager (Apify Functions)
-    stock_data = api().run_apify(actor='Yahoo Finance', input={'tickers': result['tickers']}, runtime=20)
+    with open("previous_run_date.txt", 'r') as file:
+        previous_date = file.read()
+        file.close()
+    todays_date = date.today().strftime("%Y-%m-%d")
+    apify_input = {"end_date": todays_date,"start_date": previous_date,'tickers': result['tickers']}
+    stock_data = api().run_apify(actor='Yahoo Finance', input=apify_input, runtime=20)
+    with open("previous_run_date.txt", 'w') as file:
+        file.write(todays_date)
+        file.close()
+    logger.critical(f'Prev Date: {previous_date}, Todays Date: {todays_date}')
 
     # Temp log (Until dashboard enpoint is made)
     #logger.info(stock_data)
